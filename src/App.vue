eafb0ded9d00913777808ad189a5ed5ebaa60cf2
Herilala Rakotonasolo <herilalah.rakotonasolo@gmail.com>
2023-11-01 20:13:47 +0300

    Initial configuration - add bootstrap

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente quisquam aspernatur aliquid nulla, eligendi vitae rem veniam exercitationem cumque, ullam, quo unde incidunt voluptas cupiditate quam nam amet ratione. Quas?</p>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo at repellendus eius odit nisi fuga magni facilis architecto doloremque! Ex accusamus commodi, magnam blanditiis culpa corrupti quia veniam ut eum!</p>
+</template>
